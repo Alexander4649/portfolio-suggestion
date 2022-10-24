50eb3532d79b52ef4c6a55ecad7e8b699ea0d5d9
Alexander4649 <handyasu08@gmail.com>
2022-10-24 18:38:21 +0900

    java

---
+++ practice.java
@@ -0,0 +1,23 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello world");//出力定型
+//         System.out.println("冒険");
+        
+//     }
+// }
+
+
+import java.util.*;  // 入力データの受け取りを簡単にしている
+
+public class practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.next();
+        System.out.println("text");
+    }
+}
+
+// //整数データの受け取り
+// Scanner scan = new Scanner(System.in);
+//         int number = scan.nextInt();
+//         System.out.println(number); 
